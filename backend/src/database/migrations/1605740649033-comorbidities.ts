a1d51bbf8400b0724995b1f88e028af672ea4563
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-18 21:47:06 -0300

    create migration para comorbidades

---
+++ backend/src/database/migrations/1605740649033-comorbidities.ts
@@ -0,0 +1,62 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class comorbidities1605740649033 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table(
+            {
+                name: 'comorbidities',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        unsigned: true,
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment'
+                    },
+                    {
+                        name: 'smoke',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'drink',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'hypertension',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'hypotension',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'heart_disease',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'diabetes',
+                        type: 'boolean',
+                        default: false
+                    },
+                    {
+                        name: 'asthma',
+                        type: 'boolean',
+                        default: false
+                    },
+                ]
+            }
+        ))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('comorbidities')
+    }
+
+}
