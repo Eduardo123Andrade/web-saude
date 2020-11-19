4800b44dc1e72669931750c803c3aab1e610b5da
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-19 15:31:06 -0300

    criada a função para verificar autenticação do usuario

---
+++ backend/src/middleware/check-auth.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express"
+import jwt from 'jsonwebtoken'
+
+
+export const checkAuth = (request: Request, response: Response, next: any) => {
+    try {
+        // console.log(request.params)
+        const authorization = request.headers.authorization? request.headers.authorization : ''
+        const token = authorization.split(' ')[1]
+
+        jwt.verify(token, 'secret_this_should_be_longer_okay')
+        next()
+    } catch (err) {
+        response.status(401).json({ message: 'Erro na autenticação' })
+        console.log(err)
+    }
+}
