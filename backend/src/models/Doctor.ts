4803c3825d1a87c1b5f0b7b4110d92a9d160ff4e
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-19 11:32:28 -0300

    criado o model doctor

---
+++ backend/src/models/Doctor.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('doctor')
+export class Doctor {
+    
+    @PrimaryGeneratedColumn()
+    id: number
+    
+    @Column()
+    name: string
+    
+    @Column()
+    password: string
+    
+    @Column()
+    crm: string
+
+}
