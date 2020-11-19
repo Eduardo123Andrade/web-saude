c1af63d56375bad359b7c06a6dc2586f575ef0df
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-19 20:10:05 -0300

    utilizando a função de exceção

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express'
+import 'express-async-errors'
 import './database/conections'
 import patientRouter from './routes/Patient'
 import doctorRouter from './routes/Doctor'
+
+import { errorHandler } from './errors/Handler'
 
 const app = express()
 app.use(express.json())
 
 app.use(patientRouter)
 app.use(doctorRouter)
+app.use(errorHandler)
 
 export default app 
