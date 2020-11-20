88caad4459eacafb96a6690d79d14fe8b27f3efe
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-20 15:49:17 -0300

    configurada as rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { LoginComponent } from './components/login/login.component';
+import { CreatePatientComponent } from './components/create-patient/create-patient.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateDoctorComponent } from './components/create-doctor/create-doctor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'create-patient', component: CreatePatientComponent },
+  {path: 'create-doctor', component: CreateDoctorComponent },
+  {path: 'login', component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
