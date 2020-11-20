32ac59b7bc88144ca711c1572d5f8fca34a9ab9c
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-20 17:30:29 -0300

    adicionado o cors ao backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,13 @@ import 'express-async-errors'
 import './database/conections'
 import patientRouter from './routes/Patient'
 import doctorRouter from './routes/Doctor'
+import cors from 'cors'
 
 import { errorHandler } from './errors/Handler'
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use(patientRouter)
