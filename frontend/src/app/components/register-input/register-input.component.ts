89da3daf5e5fe157902df636143df0921f22dc7c
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-20 16:39:25 -0300

    criado componente para direcionar o cadastro

---
+++ frontend/src/app/components/register-input/register-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-register-input',
+  templateUrl: './register-input.component.html',
+  styleUrls: ['./register-input.component.css']
+})
+export class RegisterInputComponent implements OnInit {
+
+  @Input() filePath: string = './../../../assets/imgs/doctor.svg'
+  @Input() componentName: string = 'Cadastro de Medicos'
+  @Input() nextRouter: string = '/create-doctor'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
