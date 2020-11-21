a6ca9921162bd9408b16b390dbd4736b209384d4
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-21 04:50:39 -0300

    implementada a função de logout no header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
-  @Component({
+@Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authListenerSubs: Subscription | undefined
+  userIsAuthenticated: boolean = false
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+
 
   ngOnInit(): void {
+    this.authListenerSubs =
+      this.authService.getAuthStatusListener()
+        .subscribe(isAuthenticated => {
+          this.userIsAuthenticated = isAuthenticated
+        })
   }
 
+  ngOnDestroy() {
+    if(this.authListenerSubs)
+      this.authListenerSubs.unsubscribe()
+  }
+
+  logout() {
+    this.authService.logout()
+  }
 }
