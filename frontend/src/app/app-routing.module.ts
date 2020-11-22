ce5ea7fdbae3a14fc38c1613de85eae12dde0070
HajimeYoru <eduardo.andrade15@hotmail.com>
2020-11-22 13:53:43 -0300

    removido rotas inacessaveis

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { ListDoctorsComponent } from './components/list-doctors/list-doctors.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { AuthGuard } from './services/auth/auth.guard';
 import { LoginComponent } from './components/login/login.component';
@@ -6,17 +5,13 @@ import { CreatePatientComponent } from './components/create-patient/create-patie
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CreateDoctorComponent } from './components/create-doctor/create-doctor.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'create-patient', component: CreatePatientComponent, canActivate: [AuthGuard] },
-  { path: 'create-doctor', component: CreateDoctorComponent, canActivate: [AuthGuard] },
-  { path: 'list-doctor', component: ListDoctorsComponent, canActivate: [AuthGuard] },
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-
 ];
 
 @NgModule({
